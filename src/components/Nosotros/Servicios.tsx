a9d06e6080ad73c93960da9c9318ca43a40ce3b1
Aux Sistemas <sistemasomnibandas@gmail.com>
2026-03-02 13:07:11 -0600

    se actulizaron cambios en nosotros y corroboraron errores

--- src/components/Nosotros/Servicios.tsx
+++ src/components/Nosotros/Servicios.tsx
@@ -7,16 +7,16 @@ export default function Servicios() {
 
   return (
     
-    <section className="w-full bg-gray-50 py-16 px-6">
-      <div className="max-w-6xl mx-auto">
+    <section className= "w-full bg-white py-10 px-6">
+      <div className="max-w-5xl mx-auto">
         
         {/* Título */}
-        <h2 className="text-center text-3xl font-bold text-blue-900 mb-12 tracking-wide">
+        <h2 className="text-center text-3xl font-bold text-blue-900 mb-8 tracking-wide">
           QUÉ HACEMOS
         </h2>
 
         {/* Cards */}
-        <div className="space-y-6">
+        <div className="space-y-4">
           {
             services.map((service) => (
             <div
@@ -24,7 +24,7 @@ export default function Servicios() {
               className="flex flex-col md:flex-row items-center bg-white rounded-2xl shadow-sm border border-gray-200 overflow-hidden hover:shadow-md transition duration-300"
             >
               {/* Imagen */}
-              <div className="md:w-1/3 w-full h-56 md:h-auto">
+              <div className="md:w-[32%] w-full h-36 md:h-32">
                 <img
                   src={service.image}
                   alt={service.title}
@@ -33,14 +33,14 @@ export default function Servicios() {
               </div>
 
               {/* Contenido */}
-              <div className="md:w-2/3 w-full p-8 flex flex-col md:flex-row justify-between items-center gap-6">
-                <p className="text-gray-700 text-lg font-medium text-center md:text-left">
+              <div className="md:w-[68%] w-full px-6 py-4 flex flex-col md:flex-row justify-between items-center gap-6">
+                <p className="text-gray-700 text-lg font-medium leading-snug max-w-md">
                   {service.title}
                 </p>
 
                 <button
                  className="
-                   w-40 h-11
+                   w-[170px] h-10
                    flex items-center justify-center
                    bg-white text-blue-800
                    rounded-full
