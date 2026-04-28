b617d61bde3dbc782c822114ca1a64129ec40793
Aux Sistemas <sistemasomnibandas@gmail.com>
2026-04-28 10:53:55 -0600

    Cambios

--- src/data/productos.ts
+++ src/data/productos.ts
@@ -14,14 +14,29 @@ export const productosList :  productoPage[] = [
                 img:"/images/bandas/cuadros/cereales.jpg",
                 titulo:"Transporte continuo y establ"
             },
+
             {
                 id:2,
+                img:"/images/bandas/cuadros/carnes.jpg",
+                titulo:"prueba de objecto ventaja"
+            },
+
+             {
+                id:3,
+                img:"/images/bandas/cuadros/ban.jpg",
+                titulo:"prueba de objecto ventaja"
+            },
+
+            {
+                id:4,
                 img:"/images/bandas/cuadros/cajas.jpg",
                 titulo:"prueba de objecto ventaja"
-            }
+            },
+
+
         ],
         aplicaciones:{
-            id:1,
+            id:2,
             img:"img.jpg",
             aplicaciones:[
                 "Líneas de producción y ensamblaje",
@@ -51,52 +66,70 @@ export const productosList :  productoPage[] = [
         },
 
     },
+
     {
-        id:10,
+        id:2,
         tipo:"transmision",
         subtitulo:"Las bandas de transmision de potencia son componentes.",
         ventajas:[
             {
                 id:1,
-                img:"ventaja_1.jpg",
+                img:"/images/productos/transmision/Ventajas/imagen1.jpg",
                 titulo:"Transporte continuo y establ"
             },
             {
                 id:2,
-                img:"ventaja_1.jpg",
+                img:"/images/productos/transmision/Ventajas/imagen2.jpg",
                 titulo:"Prueba"
             },
             {
                 id:3,
-                img:"ventaja_1.jpg",
+                img:"/images/productos/transmision/Ventajas/imagen3.jpg",
                 titulo:"Transporte continuo y establ"
             },
             {
                 id:4,
-                img:"ventaja_1.jpg",
+                img:"/images/productos/transmision/Ventajas/imagen4.jpg",
                 titulo:"Transporte continuo y establ"
+           
             },
+
+
+
+
         ],
         aplicaciones:{
-            id:1,
-            img:"img.jpg",
+            id:3,
+            img:"/images/productos/transmision/Aplicaciones/imagen1.jpg",
             aplicaciones:[
                 "Líneas de producción y ensamblaje",
                 "Procesos de empaque y clasificación",
                 "Transporte de productos a granel o unitarios"
             ]
+
         },
         tipos:[
             {
                 id:1,
-                titulo:"Bandas planas (PU / PVC / Plásticas)",
-                img:"tipo_1.jpg"
+                titulo:"Bandas dentadas",
+                img:"/images/productos/transmision/Tipos/banda1.jpg"
             },
+
+
             {
                 id:2,
-                titulo:"Bandas con perfiles (Empujadores)",
-                img:"tipo_2.jpg"
-            }
+                titulo:"Bandas sincronicas",
+                img:"/images/productos/transmision/Tipos/banda2.jpg"
+            },
+
+
+           {
+                id:3,
+                titulo:"Bandas donle dentadas",
+                img:"/images/productos/transmision/Tipos/banda3.jpg"
+            },
+
+
         ],
         industrias:{
             id:1,
